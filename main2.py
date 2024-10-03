b56a3586a08b2271020a535fc8837eb44578f865
OlgaVerZo <verkienko@gmail.com>
2024-10-03 19:30:29 +0400

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,13 @@
+print("1st program")
+print((9 ** 0.5) * 5)
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+print("3rd program")
+int1 = 2*2+2
+print(int1)
+int2 = 2*(2+2)
+print(int2)
+print(int1 == int2)
+print("4th program")
+str1 = '123.456'
+print(int(float(str1) * 10) % 10)
